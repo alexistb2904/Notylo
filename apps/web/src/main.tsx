4a983a7c895849b8e4b0bbd17b477fc8af0b8556
alexistb2904 <59259007+alexistb2904@users.noreply.github.com>
2026-08-19 20:33:58 +0200

    Load mobile editor UX overrides

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "katex/dist/katex.min.css";
 import "./styles.css";
+import "./mobile-editor.css";
 import { App } from "./App";
 import { AuthProvider } from "./lib/auth";
 
